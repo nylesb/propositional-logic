c8487465c3a98340c83082381d6ef4be6e655c25
nylesb <nbreecher@gmail.com>
2014-12-07 15:27:32 -0600

    wfp checks arguments now, tests input file for FOL added.

--- main.lisp
+++ main.lisp
@@ -1,70 +1,99 @@
 (defun wfp-checker (input)
   "Input is a string of the proposition to check.
-  Note: Quotes of the propsotion must be escaped, i.e. use \".
-  Returns t if input is a well-formed proposition, nil otherwise."
-  (let ((syntax '("NOT" "AND" "OR" "IMPLIES" "EQUIV"))
-        (stack '()))
-    ;;; Check input in two steps.  First verify parentheses and quotes are
-    ;;; properly paired. Propositions use valid Lisp syntax, so the read function
-    ;;; will give an error if the proposition is not a well-formed S-exp.
-    ;;; Then verify that the phrase only contains valid syntax and atoms.
-    (with-input-from-string (phrase input)
-      (unless (ignore-errors (read phrase))
-        (return-from wfp-checker nil)))
-    (do* ((i 0 (+ i 1)))
-      ((equal i (length input)))
-      (let* ((next-char (char input i))
-             (symbol (make-array 0 :element-type 'character
-                                   :fill-pointer 0
-                                   :adjustable t)))
-        ;; Obtain the next symbol
-        (when (equal next-char (char "(" 0))
-          (princ next-char)
-          (setf next-char (char input (setf i (+ i 1))))
-          (if (equal #\" next-char) ; Checks for quoted atomiic expressions
-              (progn (vector-push-extend next-char symbol)
-                     (loop do (setf next-char (char input (setf i (+ i 1))))
-                        (vector-push-extend next-char symbol)
-                      while (not (equal next-char #\")))
-                     (setf next-char (char input (setf i (+ i 1))))
-                     (unless (equal next-char (char ")" 0)) ; ) must follow closing quote
-                       (retun-from wfp-checker nil)))
-              (loop do ; Otherwise, just build up our symbol
-                (vector-push-extend next-char symbol)
-                (setf next-char (char input (setf i (+ i 1))))
-               while (not (member next-char (list #\Space (char "(" 0) (char ")" 0)))))))
-        
-        ;; Verify symbol is of the proper form
-        (block validate
-          (unless (member next-char (list #\Space (char "(" 0) (char ")" 0)))
-            (return-from wfp-checker nil)) ; Symbol not preceeded by (
-          (when (equal symbol "") ; Nothing to process
-            (return-from validate nil))
-          (when (and (equal (char symbol 0) #\") ; Quoted alphanumeric
-                     (equal (char symbol (- (length symbol) 1)) #\"))
-            (return-from validate nil))
-          (when (and (equal (length symbol) 1) ; Single characters
-                     (equal next-char (char ")" 0))) ; Term properly enclosed
-            (when (upper-case-p (char symbol 0)) ; Make sure uppercase!
-              (return-from validate nil)
-            (return-from wfp-checker nil))) ; Single char, not uppercase letter
-          (when (digit-char-p (char symbol 1)) ; Uppercase letter, then numbers
-            (do ((j 1 (+ j 1)))
-              ((equal j (length symbol)))
-              (unless (digit-char-p (char symbol j)) ; Oops! Not all numbers
-                (return-from wfp-checker nil)))
-            (return-from validate nil)) ; Yes!  Letter, then all numbers
-          (when (find symbol syntax :test #'equal) ; Look to be in syntax
-            (return-from validate nil))
-          (return-from wfp-checker nil)) ; All tests failed
-        
-        ;; Display what we've worked on - to use for stack later
-        (princ symbol)
-        (if (member next-char (list #\Space (char ")" 0)))
-            (princ next-char))
-        (if (equal next-char (char "(" 0)) ; Phrase lacked spaces before (
-            (setf i (- i 1))))) ; Go back to fix this
-    t))
+  Returns t if input is a well-formed proposition, nil otherwise.
+  (Since input a str, double quotes of the propsotion must be escaped, i.e. use \".)
+  (AND/OR can take any number of arguments and be a wfp still.)"
+  (labels ((verify-number (phrase)
+            "Phrase is a list representing a proposition.  Recursively verifies each
+            syntax word has proper num of arguments.  Returns nil if not."
+            (let ((term (first phrase))
+                  (rest-length (length (rest phrase))))
+              (cond ((equal term 'NOT) (if (/= rest-length 1)
+                                           (return-from verify-number nil)))
+                    ((or (equal term 'IMPLIES) (equal term 'EQUIV))
+                     (if (/= rest-length 2)
+                         (return-from verify-number nil)))
+                    ((or (equal term 'AND) (equal term 'OR))
+                     (if (< rest-length 2)
+                         (return-from verify-number nil)))
+                    ((> rest-length 0) ; A single term, nothing may follow in its group
+                     (return-from verify-number nil))))
+            (dolist (term (rest phrase))
+              (unless (verify-number term)
+                (return-from verify-number nil)))
+            t))
+    (let ((syntax '("NOT" "AND" "OR" "IMPLIES" "EQUIV")))
+      ;;; Check input in three steps.  First verify parentheses and quotes are
+      ;;; properly paired. Propositions use valid Lisp syntax, so the read function
+      ;;; will give an error if the proposition is not a well-formed S-exp.
+      ;;; Second, verify that the phrase only contains valid syntax and atoms.
+      ;;; Third, check that syntax terms have proper number of arguments.
+      
+      ;; First step - verify matching parentheses and quotes.  This step helps us
+      ;; avoid potential errors when validating bad expressions in step two.
+      (with-input-from-string (phrase input)
+        (unless (ignore-errors (read phrase))
+          (return-from wfp-checker nil)))
+      
+      ;; Second step - Verify proper terms and syntax are used.
+      (do* ((i 0 (+ i 1)))
+        ((equal i (length input)))
+        (let* ((next-char (char input i))
+               (symbol (make-array 0 :element-type 'character
+                                     :fill-pointer 0
+                                     :adjustable t)))
+          ;; Obtain the next symbol
+          (when (equal next-char (char "(" 0))
+            (setf next-char (char input (setf i (+ i 1))))
+            (if (equal #\" next-char) ; Checks for quoted atomiic expressions
+                (progn (vector-push-extend next-char symbol)
+                       (loop do (setf next-char (char input (setf i (+ i 1))))
+                          (vector-push-extend next-char symbol)
+                        while (not (equal next-char #\")))
+                       (setf next-char (char input (setf i (+ i 1))))
+                       (unless (equal next-char (char ")" 0)) ; ) must follow closing quote
+                         (retun-from wfp-checker nil)))
+                (loop do ; Otherwise, just build up our symbol
+                  (vector-push-extend next-char symbol)
+                  (setf next-char (char input (setf i (+ i 1))))
+                 while (not (member next-char (list #\Space (char "(" 0) (char ")" 0)))))))
+          
+          ;; Verify symbol is of the proper form
+          (block validate
+            (unless (member next-char (list #\Space (char "(" 0) (char ")" 0)))
+              (return-from wfp-checker nil)) ; Symbol not preceeded by (
+            (when (equal symbol "") ; Nothing to process
+              (return-from validate t))
+            (when (and (equal (char symbol 0) #\") ; Quoted alphanumeric
+                       (equal (char symbol (- (length symbol) 1)) #\"))
+              (return-from validate t))
+            (when (and (equal (length symbol) 1) ; Single characters
+                       (equal next-char (char ")" 0))) ; Term properly enclosed
+              (when (upper-case-p (char symbol 0)) ; Make sure uppercase!
+                (return-from validate t)
+              (return-from wfp-checker nil))) ; Single char, not uppercase letter
+            (when (digit-char-p (char symbol 1)) ; Uppercase letter, then numbers
+              (do ((j 1 (+ j 1)))
+                ((equal j (length symbol)))
+                (unless (digit-char-p (char symbol j)) ; Oops! Not all numbers
+                  (return-from wfp-checker nil)))
+              (return-from validate t)) ; Yes!  Letter, then all numbers
+            (when (find symbol syntax :test #'equal) ; Look to be in syntax
+              (return-from validate t))
+            (return-from wfp-checker nil)) ; All tests failed
+          
+          ;; Uncomment below to display what was worked on
+          ;;(princ symbol)
+          ;;(if (member next-char (list #\Space (char ")" 0)))
+          ;;    (princ next-char))
+          (if (equal next-char (char "(" 0)) ; Phrase lacked spaces before (
+              (setf i (- i 1))))) ; Go back to fix this
+      
+      ;; Third step - verify proper number of arguments for syntax words
+      (with-input-from-string (phrase input)
+        (unless (verify-number (read phrase))
+          (return-from wfp-checker nil)))
+      t)))
 
 (defun TruthValue (assignment wfp)
   "Evalutes wfp, a list, using rules given by assignment, which is a
@@ -99,3 +128,100 @@
       (print wfp-string)
       (with-input-from-string (in wfp-string)
         (eval (read in))))))
+
+(defun wfp-checkerFOL (input)
+  "Input is a string of the proposition to check.
+  Returns t if input is a well-formed proposition, nil otherwise.
+  (Since input a str, double quotes of the propsotion must be escaped, i.e. use \".)
+  (AND/OR can take any number of arguments and be a wfp still.)"
+  (labels ((verify-number (phrase)
+            "Phrase is a list representing a proposition.  Recursively verifies each
+            syntax word has proper num of arguments.  Returns nil if not."
+            (let ((term (first phrase))
+                  (rest-length (length (rest phrase))))
+              (cond ((equal term 'NOT) (if (/= rest-length 1)
+                                           (return-from verify-number nil)))
+                    ((or (equal term 'IMPLIES) (equal term 'EQUIV))
+                     (if (/= rest-length 2)
+                         (return-from verify-number nil)))
+                    ((or (equal term 'AND) (equal term 'OR))
+                     (if (< rest-length 2)
+                         (return-from verify-number nil)))
+                    ((> rest-length 0) ; A single term, nothing may follow in its group
+                     (return-from verify-number nil))))
+            (dolist (term (rest phrase))
+              (unless (verify-number term)
+                (return-from verify-number nil)))
+            t))
+    (let ((syntax '("NOT" "AND" "OR" "IMPLIES" "EQUIV")))
+      ;;; Check input in three steps.  First verify parentheses and quotes are
+      ;;; properly paired. Propositions use valid Lisp syntax, so the read function
+      ;;; will give an error if the proposition is not a well-formed S-exp.
+      ;;; Second, verify that the phrase only contains valid syntax and atoms.
+      ;;; Third, check that syntax terms have proper number of arguments.
+      
+      ;; First step - verify matching parentheses and quotes.  This step helps us
+      ;; avoid potential errors when validating bad expressions in step two.
+      (with-input-from-string (phrase input)
+        (unless (ignore-errors (read phrase))
+          (return-from wfp-checker nil)))
+      
+      ;; Second step - Verify proper terms and syntax are used.
+      (do* ((i 0 (+ i 1)))
+        ((equal i (length input)))
+        (let* ((next-char (char input i))
+               (symbol (make-array 0 :element-type 'character
+                                     :fill-pointer 0
+                                     :adjustable t)))
+          ;; Obtain the next symbol
+          (when (equal next-char (char "(" 0))
+            (setf next-char (char input (setf i (+ i 1))))
+            (if (equal #\" next-char) ; Checks for quoted atomiic expressions
+                (progn (vector-push-extend next-char symbol)
+                       (loop do (setf next-char (char input (setf i (+ i 1))))
+                          (vector-push-extend next-char symbol)
+                        while (not (equal next-char #\")))
+                       (setf next-char (char input (setf i (+ i 1))))
+                       (unless (equal next-char (char ")" 0)) ; ) must follow closing quote
+                         (retun-from wfp-checker nil)))
+                (loop do ; Otherwise, just build up our symbol
+                  (vector-push-extend next-char symbol)
+                  (setf next-char (char input (setf i (+ i 1))))
+                 while (not (member next-char (list #\Space (char "(" 0) (char ")" 0)))))))
+          
+          ;; Verify symbol is of the proper form
+          (block validate
+            (unless (member next-char (list #\Space (char "(" 0) (char ")" 0)))
+              (return-from wfp-checker nil)) ; Symbol not preceeded by (
+            (when (equal symbol "") ; Nothing to process
+              (return-from validate t))
+            (when (and (equal (char symbol 0) #\") ; Quoted alphanumeric
+                       (equal (char symbol (- (length symbol) 1)) #\"))
+              (return-from validate t))
+            (when (and (equal (length symbol) 1) ; Single characters
+                       (equal next-char (char ")" 0))) ; Term properly enclosed
+              (when (upper-case-p (char symbol 0)) ; Make sure uppercase!
+                (return-from validate t)
+              (return-from wfp-checker nil))) ; Single char, not uppercase letter
+            (when (digit-char-p (char symbol 1)) ; Uppercase letter, then numbers
+              (do ((j 1 (+ j 1)))
+                ((equal j (length symbol)))
+                (unless (digit-char-p (char symbol j)) ; Oops! Not all numbers
+                  (return-from wfp-checker nil)))
+              (return-from validate t)) ; Yes!  Letter, then all numbers
+            (when (find symbol syntax :test #'equal) ; Look to be in syntax
+              (return-from validate t))
+            (return-from wfp-checker nil)) ; All tests failed
+          
+          ;; Uncomment below to display what was worked on
+          ;;(princ symbol)
+          ;;(if (member next-char (list #\Space (char ")" 0)))
+          ;;    (princ next-char))
+          (if (equal next-char (char "(" 0)) ; Phrase lacked spaces before (
+              (setf i (- i 1))))) ; Go back to fix this
+      
+      ;; Third step - verify proper number of arguments for syntax words
+      (with-input-from-string (phrase input)
+        (unless (verify-number (read phrase))
+          (return-from wfp-checker nil)))
+      t)))
--- test-main.lisp
+++ test-main.lisp
@@ -2,22 +2,32 @@
 
 ;;; Perform tests for wfp-checker function
 (defparameter file (open "test-inputs/wfp-checker-tests.txt"))
+(princ (format nil "~%~%----------Test results for wfp-checker----------~%~%"))
+(do ((eof nil (not (listen file))))
+  (eof)
+  (let ((test-case (read-line file)))
+    (princ (format nil "Working on ~A~%  " test-case))
+    (princ (wfp-checker test-case))))
+
+;;; Perform tests for TruthValue function
+(defparameter file (open "test-inputs/TruthValue-tests.txt"))
+(princ (format nil "~%~%----------Test results for TruthValue----------~%~%"))
+(do ((eof nil (not (listen file))))
+  (eof)
+  (let ((test-values (read file))
+        (test-proposition (read file)))
+    (princ (format nil "Working on ~S, ~S~%  " test-values test-proposition))
+    (princ (TruthValue test-values test-proposition))))
+;; Verify that quote notation does work.  Had trouble with reading in things like
+;; '((P t)) and then passing that read value into the function
+(print (format nil "Verify that quoted inputs works properly:"))
+(print (TruthValue '((P t) (Q nil)) '(OR (P) (Q))))
+
+;;; Perform tests for wfp-checkerFOL function
+(princ (format nil "~%~%----------Test results for wfp-checkerFOL----------~%~%"))
+(defparameter file (open "test-inputs/wfp-checkerFOL-tests.txt"))
 (do ((eof nil (not (listen file))))
   (eof)
   (let ((test-case (read-line file)))
     (princ (format nil "Working on ~A~%  " test-case))
     (print (wfp-checker test-case))))
-
-;;; Perform tests for TruthValue function
-(defparameter file (open "test-inputs/TruthValue-tests.txt"))
-(do ((eof nil (not (listen file))))
-  (eof)
-  (let ((test-values (read file))
-        (test-proposition (read file)))
-    (print test-values)
-    (princ (format nil "Working on ~S, ~S~%  " test-values test-proposition))
-    (print (TruthValue test-values test-proposition))))
-;; Verify that quote notation does work.  Had trouble with reading in things like
-;; '((P t)) and then passing that read value into the function
-(print (format nil "Verify that quoted inputs works properly:"))
-(print (TruthValue '((P t) (Q nil)) '(OR (P) (Q))))
